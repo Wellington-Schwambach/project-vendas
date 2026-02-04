68f14e8e10ea9a402411543fa0903c2c949c2535
Wellington Schwambach <wellington.schwambach@gmail.com>
2026-02-03 21:22:55 -0300

    Fix(project-vendas) Ajustes gerais dos arquivos JS e criação da visualização das vendas

--- api/config/database.php
+++ api/config/database.php
@@ -10,7 +10,7 @@ class Database
             self::$conn = new PDO(
                 "pgsql:host=localhost;port=5432;dbname=project_zucchetti", // Aqui deve ser inserido o nome do seu banco de dados
                 "postgres",
-                "wellington20", // Aqui deve ser inserida a senha do seu banco de dados
+                "exemplo123", // Aqui deve ser inserida a senha do seu banco de dados
                 [
                     PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
                     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ
